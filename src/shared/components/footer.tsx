75d485a4f7117243c9846d7a2f26393e435cd478
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-02-23 19:06:27 -0300

    feat(footer): Cria footer global

--- src/shared/components/footer.tsx
+++ src/shared/components/footer.tsx
@@ -1,7 +1,34 @@
+import { FaInstagram } from "react-icons/fa";
+import { BsTwitterX } from "react-icons/bs";
+import { FaFacebook } from "react-icons/fa";
+import Image from "next/image";
 export default function Footer() {
     return(
-        <footer>
-            <p>© 2024 My App. All rights reserved.</p>
+        <footer className="w-full border-t border-borda bg-fundo-secundario">
+            <div className="max-w-6xl mx-auto px-6 py-10 flex flex-col items-center gap-3">
+
+                <Image src="/logoF.png" alt="Logo" width={58} height={40} />
+   
+                <div className="flex flex-col items-center gap-3">
+                    <h2 className="h2 text-center py-4 text-cor-primaria">
+                        Conecte-se com a Viora
+                    </h2>
+                </div>
+
+                <div className="flex gap-5">
+                    <FaInstagram className="text-cor-primaria text-xl cursor-pointer hover:scale-105 active:scale-95 transition-all duration-200" />
+                    <BsTwitterX className="text-cor-primaria text-xl cursor-pointer hover:scale-105 active:scale-95 transition-all duration-200" />
+                    <FaFacebook className="text-cor-primaria text-xl cursor-pointer hover:scale-105 active:scale-95 transition-all duration-200" />
+
+                </div>
+            </div>
+            <div className="p-2"> 
+                <p className="text-center texto-secundario">
+                    &copy; {new Date().getFullYear()} Viora. Todos os direitos reservados.
+                </p>
+                
+           
+            </div>
         </footer>
     )
 }
--- src/shared/components/header.tsx
+++ src/shared/components/header.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export default function Header() {
     return (
         <header className="w-full border-b border-borda bg-white">
-            <div className="max-w-7xl mx-auto flex items-center justify-between px-6 h-16">
+            <div className="max-w-7xl mx-auto flex items-center justify-between px-6 h-14">
 
                 <div className="flex items-center gap-8 cursor-pointer">
                     <Link href="/">
@@ -17,19 +17,20 @@ export default function Header() {
                 <div className="relative w-full max-w-md">
                     <input 
                     className="w-full h-10 p-4 pl-2 pr-10 rounded-lg border border-borda texto-secundario
-                    focus:outline-none focus:ring-1 focus:ring-destaque-suave  " 
+                    focus:outline-none focus:ring-2 focus:ring-destaque-suave  " 
                     type="text" placeholder="Buscar produtos..." />
 
                     <CiSearch className="absolute right-3 top-1/2 transform -translate-y-1/2
                      text-cor-primaria cursor-pointer" />    
                 </div>
 
-                <div className="flex items-center gap-6 pr-4">
+                <div className="flex items-center gap-6 pr-6">
+
                     <Link href="/login">
-                        <FiUser className="text-cor-primaria text-2xl cursor-pointer" />
+                        <FiUser className="text-cor-primaria text-2xl cursor-pointer hover:scale-105 active:scale-95 transition-all duration-200" />
                     </Link>
                     <Link href="/carrinho">
-                        <IoCartOutline className="text-cor-primaria text-2xl cursor-pointer" />
+                        <IoCartOutline className="text-cor-primaria text-2xl cursor-pointer hover:scale-105 active:scale-95 transition-all duration-200" />
                     </Link>
                 </div>  
 
