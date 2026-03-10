171d863ed60b167a22a902b89bd1fdd12c9bc406
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-03-10 19:07:26 -0300

    feat(ProdutosRelacionados) Cria componente Produtos Relacionados

--- src/features/produtos/components/ProdutosRelacionados.tsx
+++ src/features/produtos/components/ProdutosRelacionados.tsx
@@ -1,6 +1,20 @@
-export default function ProdutosRelacionados() {
+import CardProdutos from "./CardProdutos";
+import { ProdutoResumo } from "../type";
+import Link from "next/link";
+interface ProdutosRelacionadosProps {
+    produtos: ProdutoResumo[];
+}
+export default function ProdutosRelacionados({ produtos}: 
+    ProdutosRelacionadosProps) {
+
     return(
-        <>
-        </>
+        <section className=" grid grid-cols-3 gap-4 pt-8 pb-5 ">
+        
+            {produtos.map((produto) => (
+                <Link key={produto.id} href={`/produto/${produto.id}`}>
+                    <CardProdutos produto={produto} />
+                </Link>
+            ))}
+        </section>
     )
 }
