0648b7180798d7f960a9a041a03b4cd7cdfb6cb5
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-03-10 19:13:29 -0300

    feat(page): adiciona seção de relacionados na página de produtos detalhe

--- src/app/(store)/produto/[id]/page.tsx
+++ src/app/(store)/produto/[id]/page.tsx
@@ -2,15 +2,26 @@ import CardProdutoDetalhado from "@/features/produtos/components/CardProdutoDeta
 import { getProduto } from "@/features/produtos/services/getProduto";
 import ProdutoDescricao from "@/features/produtos/components/ProdutoDescricao";
 import ProdutosRelacionados from "@/features/produtos/components/ProdutosRelacionados";
+import { getProdutosRelacionados } from "@/features/produtos/services/getProdutosRelacionados";
+import { embaralharProdutos } from "@/shared/utils/mbaralharProdutos";
 export default async function ProdutoDetalhesPage({ params }: { params: Promise<{ id: string }> }) {
+
     const {id} = await params;
 
     const produto = await getProduto(Number(id));
+    const produtosRelacionados = await getProdutosRelacionados((produto.category));    
+    
+
+    const produtosAleatorios = embaralharProdutos(produtosRelacionados).slice(0, 6).filter(
+        (produtoIguais) => produtoIguais.id !== produto.id
+    );
+   
+    
     return(
         <main className="max-w-full mx-auto py-8 px-5">
             <CardProdutoDetalhado produto={produto} />
             <ProdutoDescricao produto={produto} />
-            <ProdutosRelacionados />
+            <ProdutosRelacionados produtos={produtosAleatorios}/>
         </main>
     )
 }
