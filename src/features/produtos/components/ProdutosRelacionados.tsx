1655e2a2b8f15a6a0324a3efa4fdf269d9d3a76a
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-03-16 18:59:00 -0300

    feat: adiciona frase produtos relacionados para melhor entedimento do usuario

--- src/features/produtos/components/ProdutosRelacionados.tsx
+++ src/features/produtos/components/ProdutosRelacionados.tsx
@@ -9,7 +9,8 @@ export default function ProdutosRelacionados({ produtos}:
 
     return(
         <section className=" grid grid-cols-3 gap-4 pt-8 pb-5 ">
-        
+            <h2 className="h2 col-span-3">Produtos Relacionados</h2>
+
             {produtos.map((produto) => (
                 <Link key={produto.id} href={`/produto/${produto.id}`}>
                     <CardProdutos produto={produto} />
