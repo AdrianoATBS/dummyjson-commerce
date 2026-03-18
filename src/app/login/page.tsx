dd314e8fa28ba36026a5a306a22b25717c888e35
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-03-18 18:50:06 -0300

    style(login): ajusta espaçamento de login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from "@/features/autenticacao/components/LoginForm";
 
 export default function Login() {
     return(
-        <main className="max-w-lg mx-auto pt-20">
+        <main className="max-w-lg mx-auto mt-20 mb-20">
             <LoginForm />
         </main>
     )
