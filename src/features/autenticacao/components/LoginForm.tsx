79aaca55e1dc0da90918559359440d3d6c357a95
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-03-18 18:44:11 -0300

    feat(autenticacao): adiciona labels e estrutura aos inputs de login

--- src/features/autenticacao/components/LoginForm.tsx
+++ src/features/autenticacao/components/LoginForm.tsx
@@ -8,14 +8,22 @@ export default function LoginForm() {
          border border-borda rounded-2xl p-6 shadow-md gap-4">
                 <h1 className="h1 font-bold bg-gradient-to-r from-hover to-cor-primaria bg-clip-text text-transparent">Login</h1>
                 
-                <InputGenerico type="text" placeholder="Digite Seu Usuario"
-                className="h3 border border-borda rounded-md px-3 py-2 shadow-sm
-                focus:outline-none focus:ring-2 focus:ring-cor-primaria "/>
-                
-                <InputGenerico type="password" placeholder="Digite Sua Senha"
-                className="h3 border border-borda rounded-md px-3 py-2 shadow-sm
-                focus:outline-none focus:ring-2 focus:ring-cor-primaria" />
+                <div className="w-full flex flex-col gap-1">
+                <label className="self-start text-lg font-medium text-cor-primaria">Usuario: </label>
 
+                    <InputGenerico type="text" placeholder="Digite Seu Usuario"
+                    className="h3 border border-borda rounded-md px-3 py-2 shadow-sm
+                    focus:outline-none focus:ring-2 focus:ring-cor-primaria "/>
+                </div>
+                
+                <div className="w-full flex flex-col gap-1">
+                    <label className="self-start text-lg font-medium text-cor-primaria">Senha: </label>
+
+                    <InputGenerico type="password" placeholder="Digite Sua Senha"
+                    className="h3 border border-borda rounded-md px-3 py-2 shadow-sm
+                    focus:outline-none focus:ring-2 focus:ring-cor-primaria" />
+                </div>
+                
                 <BotaoGenerico texto="Entrar" onClick={() => console.log("Login clicado")} 
                     className="w-full h3 border border-borda rounded-lg px-3 py-2 shadow-sm
                      cursor-pointer active:scale-95
