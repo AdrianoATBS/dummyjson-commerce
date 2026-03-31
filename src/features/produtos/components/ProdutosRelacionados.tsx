547b622034d8d8153283930667efd852da912797
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-03-31 11:41:02 -0300

    refactor(produto): ajusta estrutura para melhorar responsividade dos produtos relacionados

--- src/features/produtos/components/ProdutosRelacionados.tsx
+++ src/features/produtos/components/ProdutosRelacionados.tsx
@@ -8,14 +8,17 @@ export default function ProdutosRelacionados({ produtos}:
     ProdutosRelacionadosProps) {
 
     return(
-        <section className=" grid grid-cols-3 gap-4 pt-8 pb-5 ">
+        <section >
             <h2 className="h2 col-span-3">Produtos Relacionados</h2>
 
-            {produtos.map((produto) => (
-                <Link key={produto.id} href={`/produto/${produto.id}`}>
-                    <CardProdutos produto={produto} />
-                </Link>
-            ))}
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 pb-5">
+                {produtos.map((produto) => (
+                    <Link key={produto.id} href={`/produto/${produto.id}`}>
+                        <CardProdutos produto={produto} />
+                    </Link>
+                ))}
+
+            </div>
         </section>
     )
 }
