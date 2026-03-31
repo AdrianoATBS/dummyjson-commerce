36d02fd9cb3aa896bf6efa59b47dbfddfb423f8b
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-03-31 11:36:27 -0300

    refactor(produto): altera section para article para uma melhor semântica

--- src/features/produtos/components/CardProdutos.tsx
+++ src/features/produtos/components/CardProdutos.tsx
@@ -9,7 +9,7 @@ interface CardProdutosProps {
 
 export default function CardProdutos({ produto }: CardProdutosProps){
     return(
-        <section className="flex flex-col justify-center items-center 
+        <article className="flex flex-col justify-center items-center 
         border border-borda rounded-lg p-4 gap-2
         hover:shadow-md transition-shadow duration-300
         cursor-pointer">
@@ -17,9 +17,9 @@ export default function CardProdutos({ produto }: CardProdutosProps){
             className="rounded-lg object-cover" />
             <h2 className="h3 line-clamp-2">{produto.title}</h2>
             <p className="text-texto-secundario">⭐ Avaliação: {produto.rating}</p>
-            <p className="text-destaque-suave">Preço: ${produto.price}</p>
+            <p className="text-destaque-suave">Preço: ${produto.price.toFixed(2)}</p>
             {produto.discountPercentage > 0 && 
             <p className="text-texto-secundario">Desconto: {produto.discountPercentage}%</p>}
-        </section>
+        </article>
     )
 }
