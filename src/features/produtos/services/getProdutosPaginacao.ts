6a53b4ad52f8c20909c244cd49cce96fb225ff7d
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-04-01 14:58:45 -0300

    refactor(produto): adiciona validação da resposta na páginação de produtos

--- src/features/produtos/services/getProdutosPaginacao.ts
+++ src/features/produtos/services/getProdutosPaginacao.ts
@@ -1,16 +1,25 @@
 import fetcher  from "@/lib/fetcher";
 import { ProdutoResumo } from "../type";
-
-interface ProdutoResponse {
+import { Pagination } from "../type";
+interface ProdutoResponse extends Pagination {
     products: ProdutoResumo[];
-    total: number;
+   
 }
 
 export async function getProdutosPaginacao(pagina: number, limite: number): Promise<ProdutoResponse> {
     const skip = (pagina - 1) * limite;
     const data = await fetcher<ProdutoResponse>
     (`/products?limit=${limite}&skip=${skip}`);
+
+    if(!data || !data.products) {
+        throw new Error("Dados de produtos inválidos");
+    }
+    if(data.total === undefined) {
+        throw new Error("Total de produtos não encontrado");
+    }
+
     return data;
 
 }
+
 
