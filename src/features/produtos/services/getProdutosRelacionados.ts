13605ea4ab15c5c591340b0a7e61c0ac819f48a2
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-04-02 10:10:38 -0300

    refactor(produto): cria interface ProdutodRelacionadosResponse e adiciona validação da resposta

--- src/features/produtos/services/getProdutosRelacionados.ts
+++ src/features/produtos/services/getProdutosRelacionados.ts
@@ -1,8 +1,15 @@
 import  fetcher  from "../../../lib/fetcher";
 import { ProdutoResumo } from "../type";
-
+interface ProdutosRelacionadosResponse {
+    products: ProdutoResumo[];
+}
 
 export async function getProdutosRelacionados(categoria: string): Promise<ProdutoResumo[]> {
-    const data = await fetcher<{ products: ProdutoResumo[] }>(`/products/category/${encodeURIComponent(categoria)}`);
+    const data = await fetcher<ProdutosRelacionadosResponse>
+    (`/products/category/${encodeURIComponent(categoria)}`);
+    
+    if(!data || !Array.isArray(data.products)) {
+        throw new Error("Dados de produtos relacionados inválidos");
+    }
     return data.products;
 }
