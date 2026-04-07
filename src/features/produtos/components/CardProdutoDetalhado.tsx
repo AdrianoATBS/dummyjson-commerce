139114a3b6fe79830a2ca4628f227150a7527651
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-04-07 10:46:00 -0300

    feat(carrinho): implementa logica de adicionar produto ao carrinho e atualização de estoque

--- src/features/produtos/components/CardProdutoDetalhado.tsx
+++ src/features/produtos/components/CardProdutoDetalhado.tsx
@@ -3,13 +3,38 @@ import BotaoGenerico from "@/shared/components/BotaoGenerico";
 import { ProdutoDetalhado } from "../type";
 import Image from "next/image";
 import { GoStarFill } from "react-icons/go";
-
+import { useCarrinho } from "@/shared/hooks/useCarrinho";
+import { useState } from "react";
 interface CardProdutoDetalhadoProps {
     produto: ProdutoDetalhado;
 }
 export default function CardProdutoDetalhado({ produto }: CardProdutoDetalhadoProps) {
+    const { adicionarAoCarrinho } = useCarrinho();
+    const [estoque, setEstoque] = useState(produto.stock);
 
     const precoFinal = produto.price * (1 - produto.discountPercentage / 100);
+    const handleDiminuirEstoque = () => {
+        if(estoque > 0){
+            setEstoque(prevEstoque => prevEstoque - 1);
+        }
+    }
+    const handleAdicionarAoCarrinho = () => {
+        if(estoque == 0){
+            alert("Produto sem estoque disponível.");
+            return;
+        }
+
+        adicionarAoCarrinho({
+            id: produto.id,
+            title: produto.title,
+            price: precoFinal,
+            thumbnail: produto.thumbnail,
+            quantity: 1,
+        });
+        handleDiminuirEstoque();
+    }
+
+    
 
     return(
         <>
@@ -44,8 +69,8 @@ export default function CardProdutoDetalhado({ produto }: CardProdutoDetalhadoPr
                     <p className="h3">R$ {precoFinal.toFixed(2)}</p>
                 )}
 
-            <p className="texto-secundario">{produto.stock} unidades em estoque</p>
-            <BotaoGenerico texto="Adicionar ao Carrinho" onClick={() => alert("Produto adicionado ao carrinho!")} 
+            <p className="texto-secundario ">{estoque} unidades em estoque</p>
+            <BotaoGenerico texto="Adicionar ao Carrinho" onClick={handleAdicionarAoCarrinho} 
                 className="border border-cor-primaria text-cor-primaria hover:bg-cor-primaria
                  hover:text-white active:scale-95 max-w-2xs shadow-lg"/>
 
