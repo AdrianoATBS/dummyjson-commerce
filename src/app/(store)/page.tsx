03196969afe0a72c7a7f58b233fdb4cf757aa0cd
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-04-09 11:40:59 -0300

    refactor(produto): subsitui ProdutoGrid por ProdutosContainer na página inicial

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -1,15 +1,16 @@
 import ListaCategoria from "@/features/categoria/components/ListaCategoria";
 import { getCategoria } from "@/features/categoria/services/getCategoria";
 import ProdutoGrid from "@/features/produtos/components/ProdutoGrid";
+import ProdutosContainer from "@/features/produtos/components/ProdutosContainer";
 
 export default async function Home() {
   const categorias = await getCategoria();
-
+  
 
   return (
       <main className="max-w-7xl mx-auto px-5 ">
         <ListaCategoria categorias={categorias} />
-        <ProdutoGrid  />
+        <ProdutosContainer />
         
       </main>
 
