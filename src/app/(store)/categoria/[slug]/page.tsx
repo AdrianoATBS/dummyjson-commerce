339d34929ca5b451fefb1e4c444ec6fb10605a21
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-04-09 11:43:34 -0300

    feat(categoria): adiciona página dinâmica de categoria com listagem de produtos com slug

---
+++ src/app/(store)/categoria/[slug]/page.tsx
@@ -0,0 +1,19 @@
+import ListaCategoria from "@/features/categoria/components/ListaCategoria";
+import { getCategoria } from "@/features/categoria/services/getCategoria";
+import ProdutoGrid from "@/features/produtos/components/ProdutoGrid";
+import getProdutosPorCategoria from "@/features/categoria/services/getProdutosPorCategoria";
+import ProdutosContainer from "@/features/produtos/components/ProdutosContainer";
+
+export default async function Categoria({params}: {params: {slug: string}}) {
+    const slug = await params;
+    const categoria = await getCategoria();
+    
+    const produtos = await getProdutosPorCategoria(slug.slug);
+
+    return(
+       <>
+            <ListaCategoria categorias={categoria} />
+            <ProdutoGrid products={produtos} />
+       </>
+    )
+}
