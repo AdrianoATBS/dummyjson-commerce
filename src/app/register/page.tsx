95278f1b95ed788dfaf18f4cde27b1bf647cb154
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-05-25 15:26:18 -0300

    feat(register): adiciona novo layout da tela de cadastro

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,9 +1,13 @@
-
 import RegisterForm from "@/features/autenticacao/components/RegisterForm";
+import HeaderLoginRegistro from "@/shared/components/HeaderLoginRegistro";
 export default function Register() {
     return(
-        <main className="max-w-lg mx-auto mt-20 mb-20">
-            <RegisterForm />
+        <main className="min-h-screen w-full flex flex-col justify-between items-center">
+            <HeaderLoginRegistro />
+            <div className="w-full flex items-center justify-center flex-1 mb-12">
+
+                <RegisterForm />
+            </div>
         </main>
     )
 }
--- src/features/autenticacao/components/RegisterForm.tsx
+++ src/features/autenticacao/components/RegisterForm.tsx
@@ -4,30 +4,43 @@ import InputGenerico from "@/shared/components/InputGenerico";
 import { useState } from "react";
 import { registerUser } from "../services/postRegisterUser";
 import { useRouter } from "next/navigation";
+import { FaRegUser } from "react-icons/fa";
+import { MdLockOutline } from "react-icons/md";
+import { FaAt } from "react-icons/fa";
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa";
+import { FaGoogle } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+
+import Image from "next/image";
 import Link from "next/link";
 export  default function RegisterForm() {
 
     const router = useRouter();
 
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [age, setAge] = useState("");
+    const [nomeTodo, setNomeTodo] = useState("");
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+    const [confirmarSenha, setConfirmarSenha] = useState("");
 
-
+    const [visivelSenha, setVisivelSenha] = useState(false);
+    const [visivelConfirmarSenha, setVisivelConfirmarSenha] = useState(false);
     const handleSubmit  = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         try{
-            const ageNumber = Number(age);
+            const ageNumber = Number(senha);
             if(isNaN(ageNumber) || ageNumber <= 0){
-                throw new Error("Idade inválida. Por favor, insira um número positivo.");
+                throw new Error("Informe uma senha válida.");
             }
-            const autenticacao = await registerUser(firstName, lastName, ageNumber);
+            const autenticacao = await registerUser(nomeTodo, email, ageNumber);
             localStorage.setItem("autenticacao", JSON.stringify(autenticacao));
 
 
-            setFirstName("");
-            setLastName("");
-            setAge("");
+            setNomeTodo("");
+            setEmail("");
+            setSenha("");
+            setConfirmarSenha("");
             router.push("/");
         } catch (error) {
             console.error("Erro ao registrar usuário:", error); 
@@ -38,47 +51,135 @@ export  default function RegisterForm() {
     }
 
     return(
-        <form onSubmit={handleSubmit}
-        className="flex flex-col justify-center items-center border border-borda rounded-2xl p-6 shadow-md gap-4 ">
-            
-            <h1 className="h1 font-bold bg-gradient-to-r from-hover to-cor-primaria bg-clip-text text-transparent">Registrar</h1>
-            
-            <div className="w-full flex flex-col gap-1">
-            <label className="self-start text-lg font-medium text-cor-primaria">Nome: </label>
-
-                <InputGenerico value={firstName} onChange={(e) => setFirstName(e.target.value)}
-                type="text" placeholder="Digite seu nome" 
-                className="h3 border border-borda rounded-md px-3 py-2 shadow-sm
-                focus:outline-none focus:ring-2 focus:ring-cor-primaria" />
+        <section className="w-full max-w-4xl mx-auto 
+        overflow-hidden rounded-2xl md:grid md:grid-cols-2 " >
+            <div className=" w-full h-full 
+             " >
+               <div className="bg-linear-to-br from-[#630ED4] via-[#630ED4]/90 to-cor-primaria/80 w-full rounded-tl-2xl h-[380] flex-col
+                items-start justify-start p-8 gap-3  relative hidden md:flex ">
+                    <h2 className="h2 text-[#EDE0FF]">Viora</h2>
+                    <h3 className="h1 text-[#EDE0FF]">Bem-vindo!</h3>
+                    <p className="text-[#EDE0FF]">   
+                    Vivencie uma experiência de compras inspirada em galerias de arte
+                    onde cada detalhe é cuidadosamente selecionado para o seu
+                    gosto.</p>
+                    <Image src="/Container.png" 
+                    alt="Imagem de Registro" width={400} height={300} 
+                    />
+                    <p className="text-[#EDE0FF]">
+                        Junte-se a mais de 12.000 curadores do mundo todo.
+                    </p>
+               </div>
+               <div className="bg-[#F9F1FF] h-full w-full rounded-bl-2xl"></div>
             </div>
 
-            <div className="w-full flex flex-col gap-1">
-            <label className="self-start text-lg font-medium text-cor-primaria">Sobrenome: </label>
+            <form onSubmit={handleSubmit}
+            className="flex flex-col w-full 
+            justify-center items-start border border-borda rounded-tr-2xl rounded-br-2xl
+            p-8 shadow-md gap-4  ">
+                
+                <h1 className="h1 font-bold bg-linear-to-r from-hover
+                 to-cor-primaria bg-clip-text text-transparent">Crie sua conta</h1>
+                <p className="texto-secundario break-word max-w-prose">
+                    Insira seus dados para começar sua experiência personalizada.
+                </p>
 
-            <InputGenerico value={lastName} onChange={(e) => setLastName(e.target.value)} 
-            type="text" placeholder="Digite seu sobrenome" 
-            className="h3 border border-borda rounded-md px-3 py-2 shadow-sm
-                focus:outline-none focus:ring-2 focus:ring-cor-primaria" />
-            </div>
+                <div className="w-full flex flex-col gap-1 relative">
+                <label className="self-start text-lg font-medium text-cor-primaria">Nome Todo: </label>
+                    
+                    <FaRegUser className="absolute top-9 left-0 -translate-x-2/5 -translate-y-1/12
+                    text-texto-secundario w-4 h-4 ml-3 mt-[0.6rem] pointer-events-none" />
+                    <InputGenerico value={nomeTodo} onChange={(e) => setNomeTodo(e.target.value)}
+                    type="text" placeholder="John Doe" 
+                    className="h3 bg-[#EDE5F4] border border-borda rounded-md px-3 py-2 shadow-sm
+                    focus:outline-none focus:ring-2 focus:ring-cor-primaria pl-7" />
+                </div>
 
-            <div className="w-full flex flex-col gap-1">
-                <label className="self-start text-lg font-medium text-cor-primaria">Idade: </label>
-                    <InputGenerico value={age} onChange={(e) => setAge(e.target.value)} type="text" placeholder="Digite sua idade"
-                    className="h3 border border-borda rounded-md px-3 py-2 shadow-sm
-                    focus:outline-none focus:ring-2 focus:ring-cor-primaria" />
-            </div>
+                <div className="w-full flex flex-col gap-1 relative">
+                <label className="self-start text-lg font-medium text-cor-primaria">Email:</label>
+                    <FaAt className="absolute top-9 left-0 -translate-x-2/5 -translate-y-1/12
+                    text-texto-secundario w-4 h-4 ml-3 mt-[0.6rem] pointer-events-none" />
+                    <InputGenerico value={email} onChange={(e) => setEmail(e.target.value)} 
+                    type="email" placeholder="john.doe@example.com" 
+                    className="h3 bg-[#EDE5F4] border border-borda rounded-md px-3 py-2 shadow-sm
+                        focus:outline-none focus:ring-2 focus:ring-cor-primaria pl-7" />
+                </div>
 
-            <BotaoGenerico texto="Registrar"
-                className="w-full h3 border border-borda text-cor-primaria rounded-lg px-3 py-2 shadow-sm
-                 cursor-pointer active:scale-95
-                 hover:bg-cor-primaria hover:text-destaque-suave" />
+                <div className="w-full flex flex-row justify-between gap-4 ">
+                    <div className="flex flex-1 flex-col gap-1 relative">
+                        <label className="self-start text-lg font-medium text-cor-primaria">Senha </label> 
+                            
+                            <MdLockOutline className="absolute top-9 left-0 -translate-x-2/5 -translate-y-1/12
+                            text-texto-secundario w-4 h-4 ml-3 mt-[0.6rem] pointer-events-none" />
+                            <InputGenerico value={senha} onChange={(e) => setSenha(e.target.value)} 
+                            type={visivelSenha ? "text" : "password"}  placeholder="••••••••"
+                            className="w-full h-10 border bg-[#EDE5F4] border-borda rounded-md px-3 py-2 shadow-sm 
+                            focus:outline-none focus:ring-2 focus:ring-cor-primaria pl-7" />
+                            
+                            {visivelSenha ? (
+                                <FaEyeSlash className="absolute top-9 right-3 translate-x-2/5 -translate-y-1/12
+                                text-texto-secundario w-4 h-4 mr-3 mt-[0.6rem] cursor-pointer"
+                                onClick={() => setVisivelSenha(false)} />
+                            ): (
+                                <FaEye className="absolute top-9 right-3 translate-x-2/5 -translate-y-1/12
+                                text-texto-secundario w-4 h-4 mr-3 mt-[0.6rem] cursor-pointer"
+                                onClick={() => setVisivelSenha(true)} />
+                            )}
 
-          
-            <div className=" w-full flex justify-start ">
-                <Link className="text-lg text-cor-primaria active:scale-95 hover:text-hover"
-                href={"/login"}> ⬅ Voltar</Link>
-            </div>
-           
-        </form>
+                    </div>
+                    
+                    <div className="flex flex-1 flex-col gap-1 relative">
+                        <label className="self-start text-lg font-medium text-cor-primaria">Confirmar Senha </label>        
+                            <MdLockOutline className="absolute top-9 left-0 -translate-x-2/5 -translate-y-1/12
+                            text-texto-secundario w-4 h-4 ml-3 mt-[0.6rem] pointer-events-none" />
+                            <InputGenerico value={confirmarSenha} onChange={(e) => setConfirmarSenha(e.target.value)} 
+                            type={visivelConfirmarSenha ? "text" : "password"} placeholder="••••••••"
+                            className="w-full h-10 border bg-[#EDE5F4] border-borda rounded-md px-3 py-2 shadow-sm
+                            focus:outline-none focus:ring-2 focus:ring-cor-primaria pl-7" />
+                            
+                            {visivelConfirmarSenha ? (
+                                <FaEyeSlash className="absolute top-9 right-3 translate-x-2/5 -translate-y-1/12
+                                text-texto-secundario w-4 h-4 mr-3 mt-[0.6rem] cursor-pointer"
+                                onClick={() => setVisivelConfirmarSenha(false)} />
+                            ):(
+                                <FaEye className="absolute top-9 right-3 translate-x-2/5 -translate-y-1/12
+                                text-texto-secundario w-4 h-4 mr-3 mt-[0.6rem] cursor-pointer"
+                                onClick={() => setVisivelConfirmarSenha(true)} />
+                            )}
+                        
+                    </div>
+                </div>
+
+                <div className="w-full flex justify-start items-center gap-4">
+                    <InputGenerico type="checkbox" id="Relembre-me"
+                    className="w-4 h-4 border-2 border-borda
+                    bg-[#E8DFEE]/1120 rounded-sm cursor-pointer" />
+                    <label htmlFor="Relembre-me" className="text-sm text-texto-secundario">
+                        Eu concordo com o
+                        <Link href="/termos" className="text-cor-primaria hover:text-hover hover:underline"> Termos de Serviço</Link> 
+                        e a 
+                        <Link href="/privacidade" className="text-cor-primaria hover:text-hover hover:underline"> Política de Privacidade</Link>.
+                    </label>
+                </div>
+
+                <div className="w-full flex flex-col justify-center items-center gap-4">
+
+                <BotaoGenerico texto="Registrar conta ->" type="submit"
+                    className="w-full h-10  bg-cor-primaria
+                    text-white rounded-lg px-3 py-2 font-bold shadow-xl
+                    cursor-pointer active:scale-95
+                    hover:bg-hover" />
+
+                    <p className="text-sm text-[#4A4455]">Já tem uma conta? 
+                        <Link href="/login" className="text-cor-primaria
+                         hover:text-hover hover:underline"> Faça login</Link></p>
+                </div>
+                <div className="w-full flex justify-center items-center gap-5">
+                    <FaGoogle className="w-6 h-6 text-[#1D1A24] cursor-pointer" />
+                    <FaGithub className="w-6 h-6 text-[#1D1A24] cursor-pointer" />
+                    <FaXTwitter className="w-6 h-6 text-[#1D1A24] cursor-pointer" />
+                </div>                   
+            </form>
+        </section>
     )
 }
