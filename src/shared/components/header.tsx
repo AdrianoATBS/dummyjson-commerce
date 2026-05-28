7109cb5d8799a9e4f21dc6fa6d0f2f6872590426
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-05-28 15:34:17 -0300

    style(header): refina visual do cabeçalho

--- src/shared/components/header.tsx
+++ src/shared/components/header.tsx
@@ -5,33 +5,35 @@ import { FiUser } from "react-icons/fi";
 import Link from "next/link";
 export default function Header() {
     return (
-        <header className="w-full border-b border-borda bg-white">
+        <header className="w-full border-b border-borda bg-white shadow-md">
             <div className="max-w-7xl mx-auto flex items-center justify-between px-6 h-14">
 
-                <div className="flex items-center gap-8 cursor-pointer">
+                <div className="w-full flex items-center gap-8 cursor-pointer">
                     <Link href="/">
                      <Image src="/logoH.png" alt="Logo" width={58} height={58} 
                       />
                     </Link>
                 </div>
                 
-                <div className="relative w-full max-w-md">
+                <div className="relative w-full max-w-56 mr-5">
                     <input 
-                    className="w-full h-10 p-4 pl-2 pr-10 rounded-lg border border-borda texto-secundario
-                    focus:outline-none focus:ring-2 focus:ring-destaque-suave  " 
+                    className="w-full h-7 p-4 pl-3 pr-10 rounded-full border border-borda texto-secundario
+                    focus:outline-none bg-texto-secundario/25 " 
                     type="text" placeholder="Buscar produtos..." />
 
                     <CiSearch className="absolute right-3 top-1/2 transform -translate-y-1/2
-                     text-cor-primaria cursor-pointer" />    
+                     text-[#1D1A24] cursor-pointer" />    
                 </div>
 
                 <div className="flex items-center gap-6 pr-6">
 
-                    <Link href="/login">
-                        <FiUser className="text-cor-primaria text-2xl cursor-pointer hover:scale-105 active:scale-95 transition-all duration-200" />
-                    </Link>
                     <Link href="/carrinho">
-                        <IoCartOutline className="text-cor-primaria text-2xl cursor-pointer hover:scale-105 active:scale-95 transition-all duration-200" />
+                        <IoCartOutline className="text-[#1D1A24]  cursor-pointer
+                         hover:scale-105 active:scale-95 transition-all duration-200" />
+                    </Link>
+                    <Link href="/login">
+                        <FiUser className="text-[#1D1A24]  cursor-pointer hover:scale-105 
+                        active:scale-95 transition-all duration-200" />
                     </Link>
                 </div>  
 
