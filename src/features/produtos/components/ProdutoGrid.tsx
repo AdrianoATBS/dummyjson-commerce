3664fa969a98002de482ec467b322a9abdb18af4
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-06-15 10:14:27 -0300

    feat(produtos): melhora semântica e reutilização do componente ProdutoGrid

--- src/features/produtos/components/ProdutoGrid.tsx
+++ src/features/produtos/components/ProdutoGrid.tsx
@@ -6,8 +6,8 @@ import { ProdutoDetalhado } from "../type";
 import CardProdutoDestaque from "./CardProdutoDestaque";
 interface ProdutoGridProps {
     products: ProdutoDetalhado[];
-    produtoDestaque: ProdutoDetalhado;
-    paginaAtual: number;
+    produtoDestaque?: ProdutoDetalhado;
+    paginaAtual?: number;
 }
 
 export default function ProdutoGrid({ products, produtoDestaque, paginaAtual }: ProdutoGridProps){ 
@@ -16,7 +16,7 @@ export default function ProdutoGrid({ products, produtoDestaque, paginaAtual }: 
   const produtosExibidos = ehPrimeiraPagina ? products.slice(1,9) : products.slice(0,8);
   
   return (
-    <>
+    <section>
       <div className=" grid grid-cols-1 gap-4 pt-4 pb-4 sm:grid-cols-2 md:grid-cols-3">
         {produtoDestaque && ehPrimeiraPagina && (
             <Link href={`/produto/${produtoDestaque.id}`} key={produtoDestaque.id} 
@@ -31,6 +31,6 @@ export default function ProdutoGrid({ products, produtoDestaque, paginaAtual }: 
         ))}
     </div>
     
-    </>
+    </section>
   );
 }
